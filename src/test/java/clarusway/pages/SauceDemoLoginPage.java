7bab15f0ae8d2ec21bf3831c2e36e2ae150a61f8
Büşra Talih <busra.talihhh@gmail.com>
2023-01-07 01:21:37 +0300

    07.01.2023

--- src/test/java/clarusway/pages/SauceDemoLoginPage.java
+++ src/test/java/clarusway/pages/SauceDemoLoginPage.java
@@ -2,8 +2,11 @@ package clarusway.pages;
 
 import clarusway.utilities.Driver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class SauceDemoLoginPage {
 
@@ -28,6 +31,15 @@ public class SauceDemoLoginPage {
     @FindBy(xpath="//*[@id='login-button']")
     public WebElement btnLogin;
 
+    @FindAll(
+            {
+                    @FindBy(id = "user-name"),
+                    @FindBy(id = "password"),
+                    @FindBy(xpath = "//*[@id='login-button']")
+
+            }
+    )
+    public List<WebElement> inputElements;
 
 
 
